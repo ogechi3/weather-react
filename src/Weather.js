7b5db938c757b062dd8a44656c3b251cb712cfea
Thomas Jennifer <jsamuelt3@gmail.com>
2024-02-02 09:26:20 +0100

    created a new file and imported axios to get data

---
+++ src/Weather.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState("");
+
+  function getData(response) {
+    setLoaded(true);
+    console.log(response.data);
+    setWeather({
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      feelsLike: response.data.temperature.feels_like,
+      description: response.data.condition.description,
+      wind: response.data.wind.speed,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+    });
+  }
+
+  function handleSearch(event) {
+    event.preventDefault();
+    let apiKey = `7fa2303eo545aaff0tfbf93c4ef484a6`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(getData);
+  }
+
+  function changeCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <div>
+      <form onSubmit={handleSearch}>
+        <input
+          type="search"
+          placeholder="Enter a city"
+          autoFocus
+          onChange={changeCity}
+        />
+        <input type="submit" value="search" />
+      </form>
+    </div>
+  );
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature:{Math.round(weather.temperature)}℃</li>
+          <li>Feels like:{Math.round(weather.feelsLike)}℃</li>
+          <li>Humidity:{weather.humidity}</li>
+          <li>Wind:{weather.wind}km/h</li>
+          <li>Description:{weather.description}</li>
+          <li>
+            <img src={weather.icon} alt="" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
